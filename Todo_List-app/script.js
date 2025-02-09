fa5a3e4d2ec4b14e8a3155ab94f936ca77c7e190
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-02-09 15:29:22 +0500

    Completed the todo-list app project

--- Todo_List-app/script.js
+++ Todo_List-app/script.js
@@ -13,4 +13,25 @@ const addTask = () => {
   li.appendChild(span)
  }
  inputBox.value = "";
+ saveData();
 }
+
+listContainer.addEventListener("click", function(e){
+ if(e.target.tagName === "LI"){
+  e.target.classList.toggle("checked")
+  saveData();
+ } else if (e.target.tagName === "SPAN") {
+   e.target.parentElement.remove();
+   saveData();
+ }
+}, false)
+
+const saveData = () => {
+ localStorage.setItem("data", listContainer.innerHTML)
+}
+
+const showTask = () => {
+ listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
