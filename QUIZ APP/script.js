2644ab52097063e6098f43888ea2fec1af9b6f24
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-02-10 13:10:24 +0500

    Complete the Quiz App

--- QUIZ APP/script.js
+++ QUIZ APP/script.js
@@ -83,8 +83,41 @@ const selectAnswer = (e) => {
  const isCorrect = selectedBtn.dataset.correct === "true";
  if(isCorrect){
   selectedBtn.classList.add("correct");
+  score++;
  }else{
   selectedBtn.classList.add("incorrect");
  }
+ Array.from(answerButtons.children).forEach(button => {
+  if(button.dataset.correct === "true"){
+   button.classList.add("correct");
+  }
+  button.disabled = true;
+ });
+ nextButton.style.display = "block";
 }
+
+const showScore = () => {
+ resetState();
+ quesitonElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+ nextButton.innerHTML = "Play Again";
+ nextButton.style.display = "block"
+}
+
+const handleNextButton = () => {
+ currentQuestionIndex++;
+ if(currentQuestionIndex < questions.length){
+  showQuestion();
+ }else{
+  showScore();
+ }
+}
+
+nextButton.addEventListener("click", () => {
+ if(currentQuestionIndex < questions.length){
+  handleNextButton();
+ }else{
+  startQuiz();
+ }
+})
+
 startQuiz();
