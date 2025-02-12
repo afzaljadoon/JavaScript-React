0f736b5f1de8d048606b65b37c63f7a7c8358d4c
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-02-12 12:04:32 +0500

    Complete the Create Notes Project

--- Notes_app/script.js
+++ Notes_app/script.js
@@ -1,12 +1,69 @@
 const notesContainer = document.querySelector(".notes-container");
-const createBtn = document.querySelector(".btn")
+const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+
+const showNotes = () => {
+  notesContainer.innerHTML = localStorage.getItem("notes") || ""; 
+
+ 
+  document.querySelectorAll(".input-box").forEach(note => {
+    note.onkeyup = function () {
+      updateStorage();
+    };
+  });
+
+  document.querySelectorAll(".notes-container img").forEach(img => {
+    img.addEventListener("click", function () {
+      img.parentElement.remove();
+      updateStorage();
+    });
+  });
+};
+
+showNotes(); 
+
+
+const updateStorage = () => {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+};
+
 createBtn.addEventListener("click", () => {
- let inputBox = document.createElement("p");
- let img = document.createElement("img");
- inputBox.className = "input-box";
- inputBox.setAttribute("contenteditable", "true");
- img.src = "images/delete.png";
- notesContainer.appendChild(inputBox).appendChild(img);
-})
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "images/delete.png";
+
+  inputBox.appendChild(img);
+  notesContainer.appendChild(inputBox);
+
+ 
+  inputBox.onkeyup = function () {
+    updateStorage();
+  };
+
+ 
+  img.addEventListener("click", function () {
+    inputBox.remove();
+    updateStorage();
+  });
+
+  updateStorage(); 
+});
+
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorage();
+  }
+});
+
+document.addEventListener("keydown", event => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLineBreak");
+    event.preventDefault();
+  }
+});
