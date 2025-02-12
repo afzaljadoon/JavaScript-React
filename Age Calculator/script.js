e2a46d8957211a2d64fe0946a4e75e26c75f1476
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-02-13 00:03:17 +0500

    Complete the Age Calculator Project

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -1,5 +1,6 @@
 let userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("result");
 
 const calculateAge = () => {
  let birthDate = new Date(userInput.value);
@@ -34,7 +35,7 @@ d3 = getDaysInMonth(y1, m1) + d2 - d1;
   m3 = 11;
   y3--;
  }
- console.log(y3, m3, d3);
+ result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months and <span>${d3}</span> days old`;
 }
 
 const getDaysInMonth = (year, month) => {
