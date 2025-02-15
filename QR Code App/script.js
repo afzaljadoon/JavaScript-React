9571519f70bb64c1bad33cf9f47a25a640cb65c8
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-02-15 16:05:15 +0500

    Complete the QR code generator app

--- QR Code App/script.js
+++ QR Code App/script.js
@@ -4,5 +4,15 @@ let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 
 const generateQR = () => {
- qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.ariaValueMax;
+ if(qrText.value.length > 0){
+  qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+
+  imgBox.classList.add("show-img")
+ }else{
+  qrText.classList.add("error");
+  setTimeout(() => {
+   qrText.classList.remove("error");
+  }, 1000);
+ }
+
 }
