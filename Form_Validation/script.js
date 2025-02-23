81236e10ed2d3b3dc729884e58469c1efae0e4cd
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-02-23 15:15:51 +0500

    Customized the project

--- Form_Validation/script.js
+++ Form_Validation/script.js
@@ -19,3 +19,50 @@ if(!name.match(/^[A-Za-z]*\s[A-Za-z]*$/)){
  nameError.innerHTML = "<i class='fas fa-check-circle'></i>";
  return true;
 }
+
+const validatePhone = () => {
+ var phone = document.getElementById('contact-phone').value;
+
+ if(phone.length == 0){
+  phoneError.innerHTML = "Phone no is required";
+  return false;
+ }
+ if(phone.length !== 10){
+  phoneError.innerHTML = "Phone no should be 10 digits";
+  return false;
+ }
+ if(!phone.match(/^[0-9]{10}$/)){
+  phoneError.innerHTML = "Only digits please.";
+  return false;
+ }
+ phoneError.innerHTML ='<i class="fas fa-check-circle"></i>';
+ return true;
+}
+
+const validateEmail = () => {
+ var email = document.getElementById("contact-email").value;
+
+ if(email.length == 0){
+  emailError.innerHTML = "Email is required";
+  return false;
+ }
+ if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+  emailError.innerHTML = 'Email Invalid';
+  return false;
+ }
+
+ emailError.innerHTML ='<i class="fas fa-check-circle"></i>';
+ return true;
+}
+
+const validateMessage = () => {
+ var message = document.getElementById('contact-message').value;
+ var required = 30;
+ var left = required - message.length;
+ if(left > 0){
+  messageError.innerHTML = left + 'more characters required';
+  return false;
+ }
+ messageError.innerHTML = '<i class="fas fa-check-circle"></i>';
+ return false;
+}
