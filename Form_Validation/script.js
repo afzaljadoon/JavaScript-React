d3e1ec5ccd4584175d3a446129676074ecc492e1
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-02-23 12:57:58 +0500

    Customized the project

---
+++ Form_Validation/script.js
@@ -0,0 +1,9 @@
+var nameError = document.getElementById("name-error");
+var phoneError = document.getElementById("phone-error");
+var emailError = document.getElementById("email-error");
+var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
+
+const validateName = () => {
+ var name = document.getElementById("contact-name").value;
+}
