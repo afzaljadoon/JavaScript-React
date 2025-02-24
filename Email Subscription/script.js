085b36b3cdc5cb22a4e37c3872147fd38f09e0af
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-02-24 14:41:48 +0500

    Customized the project

---
+++ Email Subscription/script.js
@@ -0,0 +1,12 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzBXe2UfdbRk-ntS3N7SARAAnatrCy9oCp79Es4Y1xZez_scGQsyCbvT4gfwpG_cfE/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+       msg.innerHTML = "Thank  You For Subscribing!";
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
