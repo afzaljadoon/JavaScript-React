0b751b68f75b0935b68580a38dcde331b1d7e6fb
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-02-26 11:28:15 +0500

    Added some elements and design it

---
+++ BackgroundChangeEffect/script.js
@@ -0,0 +1,8 @@
+var imgBox = document.querySelector(".img-box");
+var imgWrap = document.querySelector(".img-wrap");
+
+var leftSpace = imgBox.offsetLeft;
+
+imgBox.onmousemove = function(e){
+ e.pageX;
+}
