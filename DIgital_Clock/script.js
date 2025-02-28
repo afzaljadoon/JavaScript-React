1278cfaa46a7aedc29a1348680dede2c490cbd60
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-02-28 20:01:36 +0500

    Complete the project

--- DIgital_Clock/script.js
+++ DIgital_Clock/script.js
@@ -2,6 +2,11 @@ let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
-let currentTime = new Date();
+setInterval(() => {
+ let currentTime = new Date();
 
-console.log(currentTime)
+hrs.innerHTML = (currentTime.getHours()< 10? "0": "") + currentTime.getHours();
+min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+}, 1000);
+
